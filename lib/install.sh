3055961ab47320eb832667a444e530aecb0528f6
齐伟 <qiwei03@baidu.com>
2013-12-11 17:41:44 +0800

    fix #1

--- lib/install.sh
+++ lib/install.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-git clone ssh://git@bd.ysmood.org:8022/music-fe/sofia.git
-cd sofia/lib
+dir='sofia'
+if [ $1 ]; then
+    dir="$1"
+fi
+
+git clone ssh://git@bd.ysmood.org:8022/music-fe/sofia.git "$dir"
+
+cd "$dir"/lib
+
 npm install
+
 command -v bower >/dev/null 2>&1 || { npm install -g bower; exit 1; }
 bower install
